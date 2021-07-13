46533af4659395d13e69c644f9f2c34508746b1c
xchen-me <xchen-me@users.noreply.github.com>
2021-07-12 18:38:51 -0700

    refine input

--- number-fun-app.py
+++ number-fun-app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Author: Dr. Xu Chen, University of Washington
 
+# %%
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -16,29 +17,32 @@ st.write("""
 The modulo operation, denoted as mod, returns the remainder of a division, after one number is divided by another. For example, $3 \pmod 2=1$ and $5 \pmod 3 = 2$.
 
 We create an infinity ring by the following simple mapping starting at 0:
-$x \\leftarrow (x+a) \pmod b$. 
+$x \\leftarrow (x+a) \pmod b$.
 """)
 
 st.write("Try increase the base number $b$ and change the addition $a$. See what happens!")
 
-ModBase = st.slider("Enter a base number", 1, 50, 5)
-ModAdd = st.slider("Enter an addition number", 1, 30, 2)
-
-st.write("$b=$", ModBase)
-st.write("$a=$", ModAdd)
-
-Ntick = ModBase
 radius = 0.45
 
 # draw circle
 figure, axes = plt.subplots()
 draw_circle = plt.Circle((0.5, 0.5), 0.45, fill=False)
 
-# plt.title("Circle")
 plt.axis("off")
 axes.set_aspect(1)
 
-# draw ticks
+# %%  input
+ModBaseMax = 80
+ModBase = st.slider("Enter a base number", 1, ModBaseMax, 17)
+ModAdd = st.number_input("Enter an addition number", 1, ModBaseMax-1, 8)
+# ModAdd = st.slider("Enter an addition number", 1, 30, 2)
+
+st.write("$b=$", ModBase)
+st.write("$a=$", ModAdd)
+
+Ntick = ModBase
+
+# %% draw ticks
 theta = np.linspace(2.5 * np.pi, np.pi / 2, Ntick + 1)
 a = 0.5 + radius * np.cos(theta)
 b = 0.5 + radius * np.sin(theta)
@@ -46,6 +50,7 @@ axes.plot(a, b, ".")
 axes.set_xlim([0, 1])
 axes.set_ylim([0, 1])
 
+# %% draw lines
 ii = 0
 while True:
     iiNext = (ii + ModAdd) % ModBase
@@ -63,31 +68,26 @@ while True:
     else:
         ii = iiNext
 
+# %% finishing plot
 plt.gcf().gca().add_artist(draw_circle)
 
 figure.tight_layout()
 
 st.write(figure)
-
 st.write("""
-Here is what happened: starting at $x=0$, the 12 o'clock position on the ring, we draw a line from $x$ (=0) to the remainder of $(x+a)/b$, or ( 0 +
-""", ModAdd, ") ( mod", ModBase, ") = ", ModAdd % ModBase, """
-. 
-""")
+Here is what happened: starting at $x=0$, the 12 o'clock position on the ring, we draw a line from $x$ (=0) to the remainder of $(x+a)/b$, or ( 0 + {0} ) (mod{1}) = """.format(ModAdd, ModBase), ModAdd % ModBase)
 
-st.write("The new number is then assigned to $x$, followed by the iteration: we now draw a line from",
-         ModAdd % ModBase, "to (", ModAdd, "+", ModAdd % ModBase, ")  ( mod", ModBase, ")=", (ModAdd % ModBase+ModAdd) % ModBase)
+st.write("The new number {0} is then assigned to $x$, followed by the iteration $(x+a)/b$: we now draw a line from".format(ModAdd % ModBase), ModAdd %
+         ModBase, "to ( {0} + {1} ) (mod{2}) = ".format(ModAdd % ModBase, ModAdd, ModBase), (ModAdd % ModBase+ModAdd) % ModBase)
 
 st.write("The process continues until the end point is back at the 12 o'clock position.")
 
 # %%
 st.write("""
 # From infinity to beyond: same ring, more patterns.
-""")
 
-st.write("""
 Embracing now multiplications, we create another infinity ring by the mapping:
-$y = (x \\times a) \pmod b$, where $x$ ranges from 0 to a chosen maximum. 
+$y=(x \\times a) \pmod b$, where $x$ ranges from 0 to a chosen maximum.
 """)
 
 st.write("Try increase the multiplier $a$ below and see what happens!")
@@ -136,3 +136,5 @@ plt.gcf().gca().add_artist(draw_circle)
 figure.tight_layout()
 
 st.write(figure)
+
+# %%
